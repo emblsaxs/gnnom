6c70b66dbaf3979d38f8e8a1488a8e32bdb797f0
Al Kikhney <alexey.kikhney@embl-hamburg.de>
2020-01-07 16:57:15 +0100

    Use normal distribution instead of uniform. Do not loose errors column.

--- pytools/scale-random.py
+++ pytools/scale-random.py
@@ -4,29 +4,29 @@ import saxsdocument
 import numpy as np
 import os
 
-parser = argparse.ArgumentParser(description='Randomly scales SAXS *.dat files uniformly within the given range.')
+parser = argparse.ArgumentParser(description='Randomly scales SAXS *.dat files within the given standard deviation.')
 parser.add_argument('dataPath',   metavar='path', type=str, help='path to the folder with data')
 parser.add_argument('-p', '--prefix', type=str, default="", help='add prefix to the rescaled files')
-parser.add_argument('-d', '--down', type=float,  default =0, help='low boundary for rescaling')
-parser.add_argument('-u', '--upper', type=float, default = 1, help='high boundary for rescaling')
+parser.add_argument('-s', '--stdev', type=float,  default =0.01, help='sigma (standard deviation)')
 
 
 args = parser.parse_args()
 
 inputFolder   = args.dataPath
 prefix        = args.prefix
-low           = args.down
-high          = args.upper
+stdev         = args.stdev
 
 for inputFilename in os.listdir(inputFolder):
     try:
-        scale = np.random.uniform(low, high, 1)
+        scale = np.random.normal(1.0, stdev, 1)
         doc   = saxsdocument.read(os.path.join(inputFolder, inputFilename))
         dat   = np.transpose(np.array(doc.curve[0]))
-        s  = dat[0]
-        Is = dat[1]
-        Ism = Is * scale
-        out = np.vstack((s,Ism))
+        s    = dat[0]
+        Is   = dat[1]
+        err  = dat[2]
+        errm = err*scale
+        Ism  = Is * scale
+        out  = np.vstack((s,Ism, errm))
         outPath = (prefix + inputFilename)
         np.savetxt( outPath, np.transpose(out), fmt = "%.8e")
         print(outPath + ': is written to the disk...')
