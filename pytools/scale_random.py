681e81d169f16818b1ae32ff4d1f9bd1e9fb2f0b
Al Kikhney <alexey.kikhney@embl-hamburg.de>
2020-02-12 15:53:15 +0100

    Data augmentation: noise, scale, background.

--- pytools/scale_random.py
+++ pytools/scale_random.py
@@ -7,28 +7,34 @@ import os
 parser = argparse.ArgumentParser(description='Randomly scales SAXS *.dat files within the given standard deviation.')
 parser.add_argument('dataPath',   metavar='path', type=str, help='path to the folder with data')
 parser.add_argument('-p', '--prefix', type=str, default="", help='add prefix to the rescaled files')
-parser.add_argument('-s', '--stdev', type=float,  default =0.01, help='sigma (standard deviation)')
+parser.add_argument('-n', '--noise', type=float,  default =0.0, help='sigma of Gaussian noise (standard deviation)')
+parser.add_argument('-b', '--background', type=float,  default =0.0, help='sigma of Gaussian background constant (standard deviation)')
+parser.add_argument('-s', '--scale', type=float,  default =0.0, help='sigma of Gaussian scale factor (standard deviation)')
 
 
 args = parser.parse_args()
 
 inputFolder   = args.dataPath
 prefix        = args.prefix
-stdev         = args.stdev
+noise         = args.noise
+bckgrd        = args.background
+scale         = args.scale
 
 for inputFilename in os.listdir(inputFolder):
     try:
-        scale = np.random.normal(1.0, stdev, 1)
+        mul = np.random.normal(1.0, scale, 1)
+        add = np.random.normal(0.0, bckgrd, 1)
         doc   = saxsdocument.read(os.path.join(inputFolder, inputFilename))
         dat   = np.transpose(np.array(doc.curve[0]))
         s    = dat[0]
         Is   = dat[1]
         err  = dat[2]
-        errm = err*scale
-        Ism  = Is * scale
+        nrd = np.random.normal(0.0, noise, len(Is))
+        errm = np.sqrt((err*mul)**2 + (noise*mul)**2)
+        Ism  = (Is + nrd + add) * mul
         out  = np.vstack((s,Ism, errm))
         outPath = (prefix + inputFilename)
         np.savetxt( outPath, np.transpose(out), fmt = "%.8e")
         print(outPath + ': is written to the disk...')
-    except:
-        print("Error: Could not read input data")
+    except Exception as e:
+        print(e)
