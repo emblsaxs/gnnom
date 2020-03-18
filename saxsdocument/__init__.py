c48152c531c1ba699aced759ea5749c5bcea54e9
Dmitry Molodenskiy <dmitry.molodenskiy@embl-hamburg.de>
2020-03-18 18:23:09 +0100

    Fixed __init__.py: error column was ignored.

--- saxsdocument/__init__.py
+++ saxsdocument/__init__.py
@@ -18,20 +18,26 @@ def read(fileName):
                     cols = line.split(" ")
                     cols = list(filter(lambda a: a != '', cols))
                     if is_number(cols[0]) and len(cols) >= 2:
-                        curve["s"].append(cols[0])
-                        curve["I"].append(cols[1])
+                        # we are in a number section
+                        s = float(cols[0])
+                        I = float(cols[1])
+                        curve["s"].append(s)
+                        curve["I"].append(I)
                         # if dat file with errors
                         if len(cols) == 3:
-                            curve["Err"].append(cols[2])
+                            Err = float(cols[2])
+                            curve["Err"].append(Err)
                         # if fit file with errors
                         if len(cols) == 4:
-                            curve["Err"].append(cols[2])
-                            curve["Fit"].append(cols[3])
+                            Err = float(cols[2])
+                            curve["Err"].append(Err)
+                            Fit = float(cols[3])
+                            curve["Fit"].append(Fit)
                     elif ":" in line:
                         key, val = line.split(":")[0:2]
                         properties[key] = val
                 except Exception as e:
-                    print(e)
+                    #print(e)
                     pass
         return properties, curve
     except Exception as e:
@@ -42,7 +48,7 @@ def read(fileName):
 def write(path, curve, prop=None):
     head = ""
     foot = ""
-    headerKeys = ["Sample", "Sample description"]
+    headerKeys = ["Sample", "Sample description", "parent", "Parent"]
     try:
         s = np.array(curve["s"]).astype(np.float64)
         I = np.array(curve["I"]).astype(np.float64)
@@ -52,18 +58,33 @@ def write(path, curve, prop=None):
                     st = prop[hk]
                     head += f"{hk} : {st}\n"
             for key, val in prop.items():
-                if key not in headerKeys:
-                   foot += f"{key} : {val}\n"
+                #if key not in headerKeys:
+                foot += f"{key} : {val}\n"
         if len(curve["Err"]) > 0 and len(curve["Fit"]) == 0:
             Err = np.array(curve["Err"]).astype(np.float64)
-            file = np.vstack((s, I, Err))
-        if len(curve["Err"]) > 0 and len(curve["Fit"]) > 0:
+            out = np.vstack((s, I, Err))
+        elif len(curve["Err"]) > 0 and len(curve["Fit"]) > 0:
             Err = np.array(curve["Err"]).astype(np.float64)
             Fit = np.array(curve["Fit"]).astype(np.float64)
-            file = np.vstack((s, I, Err, Fit))
-        else:
-            file = np.vstack((s, I))
-        np.savetxt(path, np.transpose(file), fmt="%.8e", header=head, footer=foot)
+            out = np.vstack((s, I, Err, Fit))
+        elif len(curve["Err"]) == 0 and len(curve["Fit"]) == 0:
+            out = np.vstack((s, I))
+        np.savetxt(path, np.transpose(out), fmt="%.8e", header=head, footer=foot)
 
     except Exception as e:
         print(f"Could not write file {path}: {e}")
+
+
+''' DEBUG
+import os
+
+folder = "C:\\Users\\Dima\\Nextcloud\\x33\\x33-dats-rg-10-100-rebin-smax-5-norm-i0"
+li = os.listdir(folder)
+
+
+for file in li:
+    path = os.path.join(folder, file)
+    print(file)
+    prop, cur = read(path)
+    write(f"C:\\Users\\Dima\\Nextcloud\\x33\\x33-dats-resaved\\{file}", cur, prop)
+'''
