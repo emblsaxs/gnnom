c1ef4dcd8da533d35b325080789347e70caaee87
Dmitry Molodenskiy <dmitry.molodenskiy@embl-hamburg.de>
2020-06-09 14:25:33 +0200

    added argparse, code refactoring.

--- pytools/addteeth.py
+++ pytools/addteeth.py
@@ -1,57 +1,50 @@
 #!/usr/bin/env python
 # coding: utf-8
+import argparse
+
+parser = argparse.ArgumentParser(description='Adds realistic errors to a synthetic data.')
+parser.add_argument('dataPath',   metavar='path', type=str, help='path to the folder with synthetic data')
+parser.add_argument('-t', '--teeth', type=str, help='path to the teeth')
+parser.add_argument('-p', '--prefix', type=str, default="err_", help='prefix added to the output files')
+
+args = parser.parse_args()
+
 
 import numpy as np
 import saxsdocument
 import os
-import sys
-import glob
-import time
 
-start_time = time.time()
-def main(prefix, teethFile, *args):
-    docT = saxsdocument.read(teethFile)
-    for file in args:
-        if "-p=" in file or "-t=" in file:
+
+__, teeth = saxsdocument.read(args.teeth)
+
+dataFiles = os.listdir(args.dataPath)
+dataFiles.sort()
+
+for inputFilename in dataFiles:
+    try:
+        if (os.path.isdir(os.path.join(args.dataPath, inputFilename))): continue 
+        prop, cur  = saxsdocument.read(os.path.join(args.dataPath, inputFilename))
+        s  = cur['s']
+        sT = teeth['s']
+        if (len(s) != len(sT)): 
+            print(f"Warning: the length of {inputFilename} is different from {args.teeth}!")
             continue
-        docI = saxsdocument.read(file)
-        datT = np.transpose(np.array(docT.curve[0]))
-        sT = datT[0]
-        IT = datT[1]
-        ET = datT[2]
-        datI = np.transpose(np.array(docI.curve[0]))
-        sI = datI[0]
-        II = datI[1]
-        if(np.array_equal(sT,sI) == False):
-            print("Error in " + file + "! Grids in dat and teeth files are different")
-            print(str((sI == sT).sum()) + " s-values out of " + str(len(sI)) + " are equivalent")
+        if(np.array_equal(s,sT) == False):
+            print(f"Warning: grid in {inputFilename} is different from {args.teeth}!")
+            print(str((s == sT).sum()) + " s-values out of " + str(len(s)) + " are equivalent")
+        Is = cur['I']
+        Err = cur['Err']
+        IsT = teeth['I']
+        ErrT = teeth['Err']
         #random_normal = np.random.normal(IT,ET)
-        EI = np.multiply(IT, np.sqrt(II))
-        II = np.random.normal(II, EI)
-        PCA_with_errors = np.vstack((sI,II, EI))
-        outFile = str(prefix) + os.path.basename(file)
-        np.savetxt(outFile, np.transpose(PCA_with_errors), fmt = "%.8e")
-        print ("New file is created ... " + outFile)
-    print("--- %s seconds ---" % (time.time() - start_time))
+        ErrI = np.multiply(IsT, np.sqrt(np.abs(Is)))
+        Is = np.random.normal(Is, Err)
+        #outCurve = np.vstack((s,Is, ErrI))
+        prop['creator'] = 'addteeth'
+        outCurve = {'s' : s, 'I' : Is, 'Err' : ErrI, 'Fit' : ''}
+        saxsdocument.write(f"{args.prefix}{inputFilename}", outCurve, prop)
 
-if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1] == "-h":
-        print("example: python addteeth.py -t=myTeeth.dat data-reduced/*.dat -p=data-reduced-with-errors/err_")
-        sys.exit("Bye")
-        raise SyntaxError("Insufficient arguments")
-    if len(sys.argv) < 3:
-        raise SyntaxError("Insufficient arguments")
-    else:
-        args = iter(sys.argv)
-        prefix = "err"
-        for arg in args:
-            if "-p" in arg:
-                prefix = arg.split("=")[1]
-            if "-t" in arg:
-                teethFile = arg.split("=")[1]
-
-        if (teethFile != ""):
-            main(prefix, teethFile, *sys.argv[2:])
-
-        else:
-            raise SyntaxError("Parse error")
+    except Exception as e:
+        print(f"Error: Could not read {inputFilename}:")
+        print(e)
+        
