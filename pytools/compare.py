149cc6a8032ad83268f94862574f5fc8dc079d3b
Dima Molodenskiy <dmitry.molodenskiy@embl-hamburg.de>
2021-03-09 11:06:51 +0100

    compare.py: Takes abs for estimating relative difference.

--- pytools/compare.py
+++ pytools/compare.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python
+"""
+Fast comparison of prediction against the ground truth.
+Using CVS files and different metrics
+"""
 import argparse
 
 parser = argparse.ArgumentParser(description='Compare NN predictions.')
@@ -12,7 +15,6 @@ args = parser.parse_args()
 
 import csv
 import numpy as np
-import os
 import matplotlib.pyplot as plt
 csv1 = args.csv1
 csv2 = args.csv2
@@ -22,7 +24,7 @@ metric        = args.metric
 ms = ['ad', 'rd', 'h', 'l']
 if metric not in ms:
     print("wrong metric! dying...")
-    os._exit(0)
+    exit()
 
 #convert to dictionaries
 with open(csv1, mode='r', newline = '') as infile:
@@ -33,30 +35,27 @@ with open(csv2, mode='r', newline = '') as infile:
     reader = csv.reader(infile)
     dict2 = {rows[0]:rows[1] for rows in reader}
 
-
-
-# find intersecions
+# find intersections
 fSet   = set(dict1)
 sSet   = set(dict2)
 sameId   = []
 for name in fSet.intersection(sSet):
     sameId.append(name)
-print(str(len(sameId)) + " out of " + str(max(len(dict1), len(dict2))) + " ids are identical")
+print(f"{len(sameId)} out of {max(len(dict1), len(dict2))} ids are identical")
 
 # write output csv
 with open(outCsvPath, mode='w') as outfile:
-    writer  = csv.writer(outfile)
-    ad      = []
-    rd      = []
-    linGT   = []
-    linC    = []
-    for num, n in enumerate(sameId, start = 1):
-        diff = round(float(dict2[n]) - float(dict1[n]), 4)
+    writer = csv.writer(outfile)
+    ad = []  # absolute difference
+    rd = []  # relative difference
+    linGT = []  # ground truth
+    linC = []  # predicted
+    for num, n in enumerate(sameId, start=1):
+        diff = round(float(dict2[n]) - float(dict1[n]), 4)  # absolute difference
         if metric == "l":
-            #writer.writerow(["#Standart deviation: ", np.std(diff)])
-            linGT.append(round(float(dict1[n]),4))
+            linGT.append(round(float(dict1[n]), 4))
             linC.append(round(float(dict2[n]), 4))
-            writer.writerow([round(float(dict1[n]),4),round(float(dict2[n]),4)])
+            writer.writerow([round(float(dict1[n]), 4), round(float(dict2[n]), 4)])
         if metric == "ad":
             writer.writerow([str(num), str(diff), n])
             ad.append(diff)
@@ -67,7 +66,7 @@ with open(outCsvPath, mode='w') as outfile:
 
 outfile.close()
 
-# if metric == 'ad' --> plot absolute differences
+#plot absolute differences
 if metric == "ad":
     plt.scatter(range(len(ad)), ad, c = 'tab:blue', alpha = 0.3, edgecolors='none')
     plt.xlabel('Number of curve')
@@ -78,18 +77,19 @@ if metric == "ad":
     plt.grid(True)
     plt.show()
 
-# if metric == 'rd' --> plot relative differences
+#plot relative differences
 if metric == "rd":
     plt.scatter(range(len(rd)), rd, c = 'tab:blue', alpha = 0.3, edgecolors='none')
     plt.xlabel('Number of curve')
     plt.ylabel('Relative difference')
+    rd = np.abs(rd)
     std = np.std(rd)
     med = np.median(rd)
     plt.title("std = " + str(std*100) + " %\nmedian = " + str(med*100) + " %")
     plt.grid(True)
     plt.show()
 
-# if metric == 'l' --> plot linear regression
+#plot linear regression
 if metric == "l":
     plt.scatter(linGT, linC, c = 'tab:blue', alpha = 0.3, edgecolors='none')
     plt.xlabel('Ground truth')
@@ -104,7 +104,7 @@ if metric == "l":
     plt.grid(True)
     plt.show()
 
-# if metric == 'h' --> plot histogram
+# plot histogram
 if metric == "h":
     # the histogram of the data
     n, bins, patches = plt.hist(rd, edgecolor='black', bins=50, density=True, facecolor='g', alpha=0.75)
