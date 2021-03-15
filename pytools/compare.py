2a527fe9161e96e5d6a3f16e7e2f20efc5bd9ca8
Dima Molodenskiy <dmitry.molodenskiy@embl-hamburg.de>
2021-03-15 11:22:27 +0100

    Rewritten in normal python.

--- pytools/compare.py
+++ pytools/compare.py
@@ -26,7 +26,7 @@ outCsvPath = args.output
 metric = args.metric
 ms = ['ad', 'rd', 'h', 'l']
 if metric not in ms:
-    print("wrong metric! dying...")
+    print("Wrong metric! dying...")
     exit()
 
 # convert to dictionaries
@@ -46,75 +46,66 @@ for name in fSet.intersection(sSet):
     sameId.append(name)
 print(f"{len(sameId)} out of {max(len(dict1), len(dict2))} ids are identical")
 
-# write output csv
-with open(outCsvPath, mode='w') as outfile:
-    writer = csv.writer(outfile)
-    ad = []  # absolute difference
-    rd = []  # relative difference
-    linGT = []  # ground truth
-    linC = []  # predicted
-    for num, n in enumerate(sameId, start=1):
-        diff = round(float(dict2[n]) - float(dict1[n]), 4)  # absolute difference
-        if metric == "l":
-            linGT.append(round(float(dict1[n]), 4))
-            linC.append(round(float(dict2[n]), 4))
-            writer.writerow([round(float(dict1[n]), 4), round(float(dict2[n]), 4)])
-        if metric == "ad":
-            writer.writerow([str(num), str(diff), n])
-            ad.append(diff)
-        if metric in ["rd", "h"]:
-            rDiff = diff / float(dict1[n])
-            writer.writerow([str(num), str(rDiff), n])
-            rd.append(rDiff)
-outfile.close()
+# calculate discrepancies
+absDiff = []  # absolute difference
+relDiff = []  # relative difference
+groundTruth = []  # ground truth
+predicted = []  # predicted
+out = []  # csv to save or to print out
+
+for num, n in enumerate(sameId, start=1):
+    GT = float(dict1[n])
+    P = float(dict2[n])
+    AD = P - GT  # absolute difference
+    RD = np.abs(AD) / GT
+    absDiff.append(AD)
+    relDiff.append(RD)
+    groundTruth.append(GT)
+    predicted.append(P)
+    if metric is "ad":
+        out.append(f"{n}, {round(AD, 3)}")
+    else:
+        out.append(f"{n}, {round(RD, 3)}")
+# compute mean error and median
+if metric is "ad":
+    aver = "{:.2%}".format(np.mean(absDiff))
+    med = "{:.2%}".format(np.median(absDiff))
+else:
+    aver = "{:.2%}".format(np.mean(relDiff))
+    med = "{:.2%}".format(np.median(relDiff))
 
 # plot absolute differences
 if metric == "ad":
-    plt.scatter(range(len(ad)), ad, c='tab:blue', alpha=0.3, edgecolors='none')
+    plt.scatter(range(len(absDiff)), absDiff, c='tab:blue', alpha=0.3, edgecolors='none')
     plt.xlabel('Number of curve')
     plt.ylabel('Absolute difference')
-    std = np.std(ad)
-    med = np.median(ad)
-    plt.title(f"std = {std * 100}%  median = {med * 100}%")
-    plt.grid(True)
-    plt.show()
 
 # plot relative differences
 if metric == "rd":
-    plt.scatter(range(len(rd)), rd, c='tab:blue', alpha=0.3, edgecolors='none')
+    plt.scatter(range(len(relDiff)), relDiff, c='tab:blue', alpha=0.3, edgecolors='none')
     plt.xlabel('Number of curve')
     plt.ylabel('Relative difference')
-    rd = np.abs(rd)
-    std = np.std(rd)
-    med = np.median(rd)
-    plt.title(f"std = {std * 100}%  median = {med * 100}%")
-    plt.grid(True)
-    plt.show()
 
 # plot linear regression
 if metric == "l":
-    plt.scatter(linGT, linC, c='tab:blue', alpha=0.3, edgecolors='none')
+    plt.scatter(groundTruth, predicted, c='tab:blue', alpha=0.3, edgecolors='none')
     plt.xlabel('Ground truth')
     plt.ylabel('Predicted')
-    linGT = np.array(linGT)
-    linC = np.array(linC)
-    rd = abs(linC - linGT) / linGT
-    aver = "{:.2%}".format(np.mean(rd))
-    med = "{:.2%}".format(np.median(rd))
-    tt = f"Mean: {aver}\nMedian: {med}"
-    plt.title(tt)
-    plt.grid(True)
-    plt.show()
 
 # plot histogram
 if metric == "h":
     # the histogram of the data
-    n, bins, patches = plt.hist(rd, edgecolor='black', bins=50, density=True, facecolor='g', alpha=0.75)
+    n, bins, patches = plt.hist(relDiff, edgecolor='black', bins=50, density=True, facecolor='g', alpha=0.75)
     plt.xlabel('Bins')
-    plt.ylabel('Probability')
-    aver = "{:.2%}".format(np.mean(np.abs(rd)))
-    med = "{:.2%}".format(np.median(np.abs(rd)))
-    tt = f"Mean: {aver}\nMedian: {med}"
-    plt.title(tt)
-    plt.grid(True)
-    plt.show()
+    plt.ylabel('Number per bin')
+
+plt.title(f"Mean: {aver}\nMedian: {med}")
+plt.grid(True)
+plt.show()
+
+# write output csv or print out
+if outCsvPath != "":
+    np.savetxt(outCsvPath, out, delimiter=",", fmt='%s')
+    print(f"{outCsvPath} is written.")
+else:
+    print("\n".join(out))
