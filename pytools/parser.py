eeb0eb59e7aba38d97766eb2921c0e90d5690278
Dima Molodenskiy <dmitry.molodenskiy@embl-hamburg.de>
2021-04-08 10:05:09 +0200

    parser.py: added possibility to plot one parameter against the other using heatmaps

--- pytools/parser.py
+++ pytools/parser.py
@@ -2,58 +2,57 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Parse crysol log files and plot histo.')
-parser.add_argument('logPath',     metavar='logs',   type=str, help='path to the training log folder')
-parser.add_argument('parameter',   metavar='par', type=str, help='mw/dmax/rg')
-parser.add_argument('outCsv', type=str,   default="out.csv", help='path to output csv file')
+parser.add_argument('logPath', metavar='logs', type=str, help='path to the training log folder')
+parser.add_argument('parameter', metavar='par', type=str, help='mw/dmax/rg')
+parser.add_argument('--secondParameter', '-par2', default=None, type=str, help='mw/dmax/rg')
 
 args = parser.parse_args()
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-logs = args.logPath
-par   = args.parameter
-fileNames = os.listdir(logs)
-print("Parsing log files...")
-outCsv     = []
-params     = []
+from utils.crysollog import parseCrysolLogs
 
-for file in fileNames:
-    f = os.path.join(logs,file)
-    with open(f, 'r') as logFile:
-      lines = logFile.readlines()
-      for line in lines:
-              if par == "rg":
-                  if "slope" in line:
-                      rg = float(line.split()[-1])
-                      params.append(rg)
-                      outCsv.append(file[:-4] + ', ' + str(round(rg, 3)))
-                      break
-              if par == "dmax":
-                  if "diameter" in line:
-                      dmax = float(line.split()[-1])
-                      params.append(dmax)
-                      outCsv.append(file[:-4] + ', ' + str(round(dmax, 3)))
-                      break         
-              if par == "mw":
-                  if "Weight" in line:
-                      mw = float(line.split()[2])/1000.0
-                      params.append(mw)
-                      outCsv.append(file[:-4] + ', ' + str(round(mw, 3)))
-                      break
-print("...done.")
+logs = []
+path = args.logPath
+par = args.parameter
+for file in os.listdir(path):
+    logs.append(os.path.join(path, file))
+params, outCsv = parseCrysolLogs(logs, par)
+pars = [item for sublist in params for item in sublist]
+# if we want eg mw vs rg
+if args.secondParameter:
+    from scipy.stats import gaussian_kde
 
-#save ground true values to csv
-outCsvPath = f"ground-{par}-{len(fileNames)}.csv"
-np.savetxt(outCsvPath, outCsv, delimiter=",", fmt='%s')
-print(outCsvPath + " is written.")
+    par2 = args.secondParameter
+    params2, __ = parseCrysolLogs(logs, par2)
+    pars2 = [item for sublist in params2 for item in sublist]
+    # plt.scatter(pars, pars2, facecolors='none', edgecolors='black')
+    xs = np.array(pars)
+    ys = np.array(pars2)
+    xy = np.vstack([xs, ys])
+    zs = gaussian_kde(xy)(xy)
+    idx = zs.argsort()
+    xs, ys, zs = xs[idx], ys[idx], zs[idx]
+    fig, ax = plt.subplots()
+    ax.set_facecolor('xkcd:black')
+    ax.scatter(xs, ys, c=zs)
+    # ax.scatter(xs, ys, c=zs, s=10, edgecolor='')
 
-n, bins, patches = plt.hist(params, edgecolor='black', bins=50, density=True, facecolor='g', alpha=0.75)
-plt.xlabel('Bins')
-plt.ylabel('Number')
-aver = round(np.mean(params), 2)
-minim = round(min(params), 2)
-maxim = round(max(params), 2)   
-tt = f"{par}\nMean: {aver}\nMin: {minim}  Max: {maxim}"
-plt.title(tt)
-plt.grid(True)
-plt.show()
+    ax.set(xlabel=par, ylabel=par2)
+    plt.grid(True)
+    plt.show()
+else:
+    # save ground true values to csv
+    outCsvPath = f"ground-{par}-{len(logs)}.csv"
+    np.savetxt(outCsvPath, outCsv, delimiter=",", fmt='%s')
+    print(outCsvPath + " is written.")
+    n, bins, patches = plt.hist(pars, edgecolor='black', bins=50, density=True, facecolor='g', alpha=0.75)
+    plt.xlabel('Bins')
+    plt.ylabel('Number')
+    aver = round(np.mean(pars), 2)
+    minim = round(np.min(pars), 2)
+    maxim = round(np.max(pars), 2)
+    tt = f"{par}\nMean: {aver}\nMin: {minim}  Max: {maxim}"
+    plt.title(tt)
+    plt.grid(True)
+    plt.show()
