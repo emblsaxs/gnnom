bcfa22e8bca3a8b0e2cc14b708f33775bb36d685
Dima Molodenskiy <dmitry.molodenskiy@embl-hamburg.de>
2022-01-14 11:43:14 +0100

    apply_nn.py:DARA version

--- gnnom/apply_nn.py
+++ gnnom/apply_nn.py
@@ -1,15 +1,3 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """
 Apply NN for prediction MW and Dmax.
 Data are first resampled to get estimation of uncertainties
@@ -22,18 +10,19 @@ parser.add_argument('type', type=str, help='p (protein), idp (intrinsically diso
 parser.add_argument('parameter', type=str, help='mw (molecular weight) or dmax (maximum intraparticle distance)')
 parser.add_argument('dataPath', metavar='path', type=str, help='path to the data file')
 parser.add_argument('I0', type=float, help='intensity in origin from AUTORG')
-parser.add_argument('Rg', type=float, help='radius of gyration from AUTORG')
-parser.add_argument('--units', type=str, default='NANOMETER', help='angular units: ANGSTROM or NANOMETER')
-parser.add_argument('--n', default=10000, type=int, help='how many times to resample')
+parser.add_argument('Rg', type=float, help='radius of gyration from AUTORRG')
+parser.add_argument('--units', type=str, default='nanometer', help='angular units: angstrom or nanometer')
+parser.add_argument('--n', default=1000, type=int, help='how many times to resample')
 # parser.add_argument('-o', '--output', type=str, default="", help='prefix to output CSV files')
 
 args = parser.parse_args()
+import os
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from keras.models import model_from_json
 from mysaxsdocument import saxsdocument
 import numpy as np
 import json
-import os
 import sys
 # from normalisation.meanvariance import normalise
 import matplotlib
@@ -43,9 +32,8 @@ import matplotlib.pyplot as plt
 from IPython.display import set_matplotlib_formats
 
 set_matplotlib_formats('svg')
-import time
+#from scipy import stats
 
-start = time.time()
 smax3 = 1.0
 smax2 = 4.980390e-01
 smax1 = 1.960780e-01
@@ -58,13 +46,14 @@ if mType not in ['p', 'idp', 'na']:
 par = args.parameter
 if par not in ['mw', 'dmax']:
     parser.error("Wrong Parameter! Please choose between mw and dmax.")
-units = args.units.upper()
-if units not in ['ANGSTROM', 'NANOMETER']:
+units = args.units
+if units not in ['angstrom', 'nanometer']:
     parser.error("Wrong units! Please choose between ANGSTROM and NANOMETER.")
 n = args.n
 inputFilename = args.dataPath
 I0 = args.I0
 Rg = args.Rg
+
 
 def normalise(Is, divisor=None, subtractor=None):
     """Normalise data as x - <x> / Var(x)"""
@@ -84,14 +73,13 @@ def normalise(Is, divisor=None, subtractor=None):
 try:
     cur, __ = saxsdocument.read(inputFilename)
     s = cur['s']
-    if units == "NANOMETER":
+    if units == "nanometer":
         s = [ss / 10.0 for ss in s]  # change to angstroms
         Rg = Rg / 10.0
-    smin = s[0]
-    smax = s[-1]
+    smin = min(s)
+    smax = max(s)
     if smin > smin0:
-        print(f"Error: Insufficient angular range!"
-              f"smin = {smin} > {smin0} A^-1")
+        print(f"Error: Insufficient angular range! smin = {smin} > {smin0} A^-1")
         sys.exit(0)
     if smax >= smax3:
         lastIndex = 256
@@ -104,7 +92,7 @@ try:
               f"smax = {smax} < {smax1} A^-1")
         sys.exit(0)
     I = np.divide(cur['I'], I0)
-    Err = cur['Err']
+    Err = np.divide(cur['Err'], I0)
 
 except Exception as e:
     print(f"Error: Could not read {inputFilename}:")
@@ -119,17 +107,17 @@ try:
     jsonFile = open(jsonFilename, 'r')
     loadedModelJson = jsonFile.read()
     json_data = json.loads(loadedModelJson)
-    mw_kda = 1.0
     # Optional fields in json
+    mw_kda = 1.0
     if 'Normalization coefficient' in json_data:
         multiplier = float(json_data['Normalization coefficient'])
     if 'meanIs' in json_data:
         meanIs = json_data['meanIs']
         stdIs = json_data['stdIs']
     elif 'meanIs' not in json_data:
-        print(f"WARNING! "
-              f"{jsonFilename} does not contain normalization coefficients!"
-              f"Proceeding without normalization...")
+        # print(f"WARNING! "
+        #      f"{jsonFilename} does not contain normalization coefficients!"
+        #      f"Proceeding without normalization...")
         mw_kda = 0.001
     # Compulsory fields in json
     smin = (float)(json_data['smin'])
@@ -143,9 +131,9 @@ try:
     h5Filename = modelPath + ".h5"
     loadedModel.load_weights(h5Filename)
     inputLength = loadedModel.input_shape[1]  # I(s) points
-    print(f"Expected input: {inputLength} points.")
+    # print(f"Expected input: {inputLength} points.")
     # outputLength = loadedModel.output_shape[1]  # p(r) points
-    print("Model loaded. Yeah!")
+    #print("Model loaded. Yeah!")
 
 except KeyError as e:
     raise Exception(f"Error: Oops, model cannot be loaded! Missing value: {e}")
@@ -177,46 +165,64 @@ for sm in sModel:
 # plt.plot(sNew, np.log10(INew), c='red')
 # plt.show()
 # saxsdocument.write("SASDH39-regrid3.dat", {'s': sNew, 'I': INew, 'Err': ErrNew})
-
-# resample n times and run nn to do prediction
+# start = time.time()
+# resample n times and run nn to do prediiction
+p = []
 data = []
 for i in range(n):
     Is = np.random.normal(INew, ErrNew)
-    # saxsdocument.write(f"{inputFilename}-resample.dat", {"s": sNew, "I": Is, 'Err': ErrNew})
+    # saxsdocument.write(f"{inputFilename}-resample-{i}.dat", {"s": sModel, "I": Is, 'Err': ErrNew})
     # exit()
     try:
         Is, __, __ = normalise(Is, stdIs, meanIs)
         data.append(Is)
-    except BaseException as e:
-        # print(f"Cannot normalise the data: {e}")
+    except:
         data.append(Is)
         pass
 
 data = np.array(data)
 p = loadedModel.predict_on_batch(data)
-p = np.round(multiplier * p, 3) * mw_kda
-if par == 'mw': p = p[p > 0]
-if par == 'dmax': p = p[p > 2 * Rg]
-
+p = multiplier * p * mw_kda
+if par == 'mw':
+    p = p[p > 0]
+elif par == 'dmax':
+    p = p[p > 2 * Rg]
+# end = time.time()
+# print(f"{par} mean : {round(np.mean(p), 2)}\n"
+#      f"{par} std  : {round(np.std(p), 2)}")
+# print(f"TOTAL TIME: {end - start}")
 # num, bins, patches = plt.hist(p, edgecolor='black', bins=50, density=True, facecolor='g', alpha=0.75)
 plt.hist(p, edgecolor='black', bins=50, density=False, facecolor='g', alpha=0.75)
-plt.xlabel(f"{par}")
+if (par == 'mw'):
+    xaxis = "Molecular weight, kDa"
+elif (par == "dmax"):
+    xaxis = "Maximum intra-particle distance, A"
+plt.xlabel(xaxis)
 plt.ylabel('Number')
-aver = np.mean(p)
-std = np.std(p)
-minim = np.min(p)
-maxim = np.max(p)
-print(f"{par} mean : {aver:.2f}\n"
-      f"{par} std  : {std:.2f}")
+aver = round(np.mean(p), 1)
+std = round(np.std(p), 1)
+minim = round(np.min(p), 1)
+maxim = round(np.max(p), 1)
+median = round(np.median(p), 1)
+# interval95 = np.round(stats.t.interval(0.95, len(p) - 1, aver, std), 1)
+interval95 = np.round(np.percentile(p, [2.5, 97.5]), 1)
+# print to parse - 1 - mw or dmax 2 - median 3 - percentiles
+print(par)
+print(median)
+print(f"[{interval95[0]} - {interval95[1]}]")
+
 if mType == 'p':
     t = "Globular proteins"
 elif mType == 'idp':
     t = "Intrinsically disordered proteins"
 elif mType == 'na':
     t = "Nucleic acids"
-tt = f"{t}:  {par}\nAverage:  {aver:.2f}  Std:  {std:.2f}\nMin: {minim:.2f}  Max: {maxim:.2f}"
+tt1 = f"{t}:  {par}\n$\mu = {{{aver:.1f}}}; \sigma\ = {{{std:.1f}}}$\n"
+tt2 = f"Range: [{minim:.1f}-{maxim:.1f}]\n"
+tt3 = f"3$\sigma$ (confidence interval 99.7%):"
+tt4 = f"{aver} $\pm\ {{{round(3 * std, 1)}}}$"
+tt = tt1 + tt2  # + tt3 + tt4
 plt.title(tt)
 plt.grid(True)
-plt.savefig(f"{mType}-{par}-{n}.svg", bbox_inches='tight')
-end = time.time()
-print(f"{(end - start):.2f}")
+folder = os.path.dirname(inputFilename)
+plt.savefig(f"{folder}/{mType}-{par}-{n}.svg", bbox_inches='tight')
