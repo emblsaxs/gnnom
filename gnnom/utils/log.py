ff96bb01db227780fcadb424b42c1507a4172221
Dima <33123184+DimaMolod@users.noreply.github.com>
2022-01-27 11:06:26 +0100

    Simplify logger messages

--- gnnom/utils/log.py
+++ gnnom/utils/log.py
@@ -1,13 +1,13 @@
-#from __future__ import print_function, absolute_import # Compatibility with python 2 and 3
-import numpy, time, sys, inspect
-
+import time, sys, inspect
 import logging
+
 logger = logging.getLogger(__name__)
 
 log_and_raise_error = lambda logger, message: log(logger, message, lvl="ERROR", exception=RuntimeError, rollback=2)
-log_warning = lambda logger, message: log(logger, message, lvl="WARNING", exception=None, rollback=2)
-log_info = lambda logger, message: log(logger, message, lvl="INFO", exception=None, rollback=2)
-log_debug = lambda logger, message: log(logger, message, lvl="DEBUG", exception=None, rollback=2)
+log_warning         = lambda logger, message: log(logger, message, lvl="WARNING", exception=None, rollback=2)
+log_info            = lambda logger, message: log(logger, message, lvl="INFO", exception=None, rollback=2)
+log_debug           = lambda logger, message: log(logger, message, lvl="DEBUG", exception=None, rollback=2)
+
 
 def log(logger, message, lvl, exception=None, rollback=1):
     logcalls = {"ERROR": logger.error,
@@ -18,7 +18,7 @@ def log(logger, message, lvl, exception=None, rollback=1):
         print(f"{lvl} is an invalid logger level.")
         sys.exit(1)
     logcall = logcalls[lvl]
-    
+
     if (logger.getEffectiveLevel() >= logging.INFO) or rollback is None:
         # Short output
         msg = f"{message}"
@@ -29,15 +29,13 @@ def log(logger, message, lvl, exception=None, rollback=1):
             # Rolling back in the stack, otherwise it would be this function
             func = func.f_back
         code = func.f_code
-        msg = "%s\n\t=> in \'%s\' function \'%s\' [%s:%i]" % (message,
-                                                              func.f_globals["__name__"],
-                                                              code.co_name, 
-                                                              code.co_filename, 
-                                                              code.co_firstlineno)
-    logcall("%s:\t%s" % (lvl,msg))
+        msg = f"{message} => in {func.f_globals['__name__']} function {code.co_name} => {code.co_filename}: {code.co_firstlineno}"
+
+    logcall(f"{lvl}: {msg}")
     if exception is not None:
         raise exception(message)
-        
+
+
 def log_execution_time(logger):
     def st_time(func):
         def st_func(*args, **keyArgs):
@@ -49,12 +47,14 @@ def log_execution_time(logger):
                 module = inspect.getmodule(func)
                 line = inspect.getsourcelines(func)[1]
                 loc = "\'%s\' [%s:%i]" % (func.__name__,
-                                         filename,
-                                         line)
+                                          filename,
+                                          line)
             except TypeError:
                 loc = "\'%s\'" % func.__name__
-            msg = "Execution time = %.4f sec\n\t=> in function %s" % (t2 - t1,loc)
+            msg = "Execution time = %.4f sec\n\t=> in function %s" % (t2 - t1, loc)
             log(logger, msg, "DEBUG", exception=None, rollback=None)
-            return r        
+            return r
+
         return st_func
+
     return st_time
